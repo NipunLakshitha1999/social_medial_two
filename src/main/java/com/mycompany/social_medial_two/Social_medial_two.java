a39fb6137034fa36ab5260272c4f5da8be50eb38
Nipun Lakshitha <nipunlakshithasilva1999@gmail.com>
2022-05-29 09:32:14 +0530

    login and home screen created

---
+++ src/main/java/com/mycompany/social_medial_two/Social_medial_two.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.social_medial_two;
+
+/**
+ *
+ * @author Nipun Lakshitha
+ */
+public class Social_medial_two {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
