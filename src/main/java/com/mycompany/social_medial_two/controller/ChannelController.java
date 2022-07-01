4bf4f8b21aced2d2e9c763e5dab3565e6f519261
Nipun Lakshitha <nipunlakshithasilva1999@gmail.com>
2022-07-01 23:20:36 +0530

    channel class and channel test class created

---
+++ src/main/java/com/mycompany/social_medial_two/controller/ChannelController.java
@@ -0,0 +1,14 @@
+/**
+ * @author Nipun Lakshitha <nipunlakshithasilva1999@gmail.com>
+ * @since 8/07/21
+ */
+
+package com.mycompany.social_medial_two.controller;
+
+public class ChannelController {
+
+    public boolean addChannelContent(String content){
+
+        return false;
+    }
+}
