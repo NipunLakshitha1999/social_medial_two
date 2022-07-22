92be04dccab54ee64b751099bf49152500ad26aa
Nipun Lakshitha <nipunlakshithasilva1999@gmail.com>
2022-07-22 21:55:38 +0530

    UI upgraded

--- src/main/java/com/mycompany/social_medial_two/view/Channel.java
+++ src/main/java/com/mycompany/social_medial_two/view/Channel.java
@@ -3,13 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.mycompany.social_medial_two.view;
-import com.mycompany.social_medial_two.obsever.SocialObserverble;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import com.mycompany.social_medial_two.obsever.SocialObserverble;
 
 /**
  *
@@ -17,57 +12,146 @@ import java.util.ArrayList;
  */
 public class Channel extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Channel
-     */
-    private JButton btnContentAdd;
-    private JButton btnSubcribe;
-    private JButton btnUnSubcribe;
-    private JTextField txtContent;
-    private JTextField txtSubsciberName;
+
     private SocialObserverble socialObserverble;
 
     public Channel() {
+        setTitle("Channel");
         socialObserverble = new SocialObserverble();
+        initComponents();
+    }
 
-        setSize(300,300);
-        setTitle("Water Tank Window");
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setLayout(new FlowLayout());
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
 
-        txtContent = new JTextField(null,null,20);
-        txtSubsciberName = new JTextField(null,null,20);
-        btnContentAdd = new JButton("Add");
-        btnSubcribe = new JButton("Subscribe");
-        btnUnSubcribe = new JButton("UnSubscribe");
+        txtContent = new javax.swing.JTextField();
+        btnAddContent = new javax.swing.JButton();
+        txtSunscribe = new javax.swing.JTextField();
+        btnSubscribe = new javax.swing.JButton();
+        btnUnsubscribe = new javax.swing.JButton();
 
-        btnSubcribe.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               socialObserverble.subscribeChannel(new ShowContent(txtSubsciberName.getText().toString()),txtSubsciberName.getText());
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        txtContent.setToolTipText("");
+        txtContent.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        btnAddContent.setBackground(new java.awt.Color(0, 102, 255));
+        btnAddContent.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnAddContent.setForeground(new java.awt.Color(255, 255, 255));
+        btnAddContent.setText("Add Content");
+        btnAddContent.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        btnAddContent.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddContentActionPerformed(evt);
             }
         });
 
-        btnUnSubcribe.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                socialObserverble.unSubsribeChannel(txtSubsciberName.getText());
+        txtSunscribe.setToolTipText("Enter User Name");
+        txtSunscribe.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        btnSubscribe.setBackground(new java.awt.Color(255, 51, 51));
+        btnSubscribe.setForeground(new java.awt.Color(255, 255, 255));
+        btnSubscribe.setText("Subscribe");
+        btnSubscribe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSubscribeActionPerformed(evt);
             }
         });
 
-        btnContentAdd.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                socialObserverble.sendContent(txtContent.getText());
+        btnUnsubscribe.setBackground(new java.awt.Color(204, 204, 204));
+        btnUnsubscribe.setText("Unsubscibe");
+        btnUnsubscribe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUnsubscribeActionPerformed(evt);
             }
         });
 
-        add(txtContent);
-        add(btnContentAdd);
-        add(txtSubsciberName);
-        add(btnSubcribe);
-        add(btnUnSubcribe);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(187, 187, 187)
+                        .addComponent(btnAddContent, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(101, 101, 101)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtSunscribe, javax.swing.GroupLayout.PREFERRED_SIZE, 266, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtContent, javax.swing.GroupLayout.PREFERRED_SIZE, 266, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnSubscribe, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnUnsubscribe, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(90, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(txtContent, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnAddContent, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69)
+                .addComponent(txtSunscribe, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSubscribe)
+                    .addComponent(btnUnsubscribe))
+                .addContainerGap(64, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnUnsubscribeActionPerformed(java.awt.event.ActionEvent evt) {
+        socialObserverble.unSubsribeChannel(txtSunscribe.getText());
+    }
+
+    private void btnAddContentActionPerformed(java.awt.event.ActionEvent evt) {
+        socialObserverble.sendContent(txtContent.getText());
+    }
+
+    private void btnSubscribeActionPerformed(java.awt.event.ActionEvent evt) {
+        socialObserverble.subscribeChannel(new ShowContent(txtSunscribe.getText().toString()),txtSunscribe.getText());
     }
 
 
+    public static void main(String args[]) {
+
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Channel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Channel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Channel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Channel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Channel().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton btnAddContent;
+    private javax.swing.JButton btnSubscribe;
+    private javax.swing.JButton btnUnsubscribe;
+    private javax.swing.JTextField txtContent;
+    private javax.swing.JTextField txtSunscribe;
 }
